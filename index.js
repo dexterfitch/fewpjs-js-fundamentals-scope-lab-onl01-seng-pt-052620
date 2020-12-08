88498757ad3a1b186962084817f1422535a99c2d
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 22:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal(animal) {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+function yourAnimal() {
+  let animal = "cat"
   return animal
 }
 
